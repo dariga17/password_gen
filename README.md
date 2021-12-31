d88fd3a86506c3a14892834dad139beda29c285f
Dariga Iskakova <darigaiskakova@icloud.com>
2021-12-31 14:40:06 -0500

    added password options to javascript

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Password Generator Starter Code
+# Password Generator
+
+ ## Purpose
+A website that offers fitness training services. 
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Website
+
+
+## Contribution
+Made with ❤️ by Dariga Iskakova
+
+### ©️2021 Password Generator. 
--- script.js
+++ script.js
@@ -13,5 +13,47 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var pwLength = prompt("How long do you want your password? (8-128)")
+  
+  if(pwLength < 8 || pwLength > 128) {
+    return generatePassword()
+  }
+  var chosenChars = ""
+  var lowerChars = "abcdefghijklmnopqrstuvwxyz"
+  var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numbers = "0123456789"
+  var symbols = "~!#$%&'()*+,-./:;<=>?@[^_`{|}" 
+  
+  var includeLower = confirm("Do you want to include lowercase characters?"); 
+  if(includeLower) {
+    chosenChars += lowerChars
+  }
+
+  var includeUpper = confirm("Do you want to include uppercase characters?"); 
+  if(includeUpper) {
+    chosenChars += upperChars
+  }
+
+  var includeSymbol = confirm("Do you want to include symbols?"); 
+  if(includeSymbol) {
+    chosenChars += symbols
+  }
+
+  var includeNumber = confirm("Do you want to include numbers?"); 
+  if(includeNumber) {
+    chosenChars += numbers
+  }
+
+  var randomizedPw = ""
+  for(i=0; i<pwLength; i++) {
+    var randomIndex = Math.floor(Math.random() * chosenChars.length)
+    var randomizedChar = chosenChars[randomIndex]
+    randomizedPw += randomizedChar
+  }
+
+  return randomizedPw
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
